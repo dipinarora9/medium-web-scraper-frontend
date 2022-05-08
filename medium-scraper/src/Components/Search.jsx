d6a6458ccb2c1530c011b3314239655543952fd5
Dipin Arora <44615649+dipinarora9@users.noreply.github.com>
2022-05-08 16:38:44 +0530

    update code to show crawling in realtime

--- medium-scraper/src/Components/Search.jsx
+++ medium-scraper/src/Components/Search.jsx
@@ -4,7 +4,9 @@ import axios from 'axios';
 function Search() {
     const [tag, setTag] = useState("");
     const [posts, setPosts] = useState([]);
-    const NGROK_URL = "f17b-103-48-197-159";
+    let crawledPostsCount = -1;
+
+    const NGROK_URL = "218d-103-48-197-159";
 
     const onFormSubmit = async (e) => {
         e.preventDefault(); // to prevent refresh
@@ -12,7 +14,9 @@ function Search() {
 
 
     const fetchPostsUrls = async () => {
-
+        setPosts([]);
+        console.log("request sent");
+        crawledPostsCount = -1;
         let response = await axios.get(`http://${NGROK_URL}.ngrok.io/search/${tag}`, {
             crossDomain: true,
         });
@@ -20,24 +24,30 @@ function Search() {
         const post_urls = response.data.post_urls
         // display crawling in frontend for length of posts
         for (let i = 0; i < post_urls.length; i++) {
-            console.log(post_urls[i]);
-            let post = { id: post_urls[i].split("-").at(-1), title: "crawling" };
-            console.log(post);
+            let post = {
+                id: i,
+                title: "crawling"
+            };
             setPosts(posts => [...posts, post]);
+
         }
         fetchPosts(post_urls);
     }
 
     const fetchPosts = async (postUrls) => {
-        setPosts([]);
-
         const newSocket = new WebSocket(`ws://${NGROK_URL}.ngrok.io/crawl`);
         newSocket.addEventListener('open', (_) => {
             newSocket.send(JSON.stringify(postUrls));
         });
+
         newSocket.onmessage = message => {
             const post = JSON.parse(message.data);
-            setPosts(posts => [...posts, post]);
+
+            setPosts(posts => {
+                posts[crawledPostsCount] = post;
+                return [...posts]
+            });
+            crawledPostsCount++;
         }
     }
 
@@ -49,12 +59,13 @@ function Search() {
                 await fetchPostsUrls();
             }}> Send </button>
             <br />
+
             {posts.map((post) =>
                 <div key={post.id}>
                     <br />
                     {post.title}
                     <br />
-                </div>
+                </div >
             )}
         </form>
     )
